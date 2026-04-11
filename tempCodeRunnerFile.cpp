d2f8f7b9b9d11450414f46db9e2664fe93c0d6ef
cavendish911 <door6527@gmail.com>
2026-04-11 22:31:33 +0200

    nothing

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,15 +1,1 @@
-#include <iostream>
-using namespace std;
 
-int main() {
-    float carbohydrates,fats,protien,totalcalories;
-    cout<<"enter carbohydrates";
-    cin>>carbohydrates;
-    cout<<"enter fats ";
-    cin>>fats;
-    cout<<"Enter protien";
-    cin>>protien;
-    totalcalories=3*carbohydrates+15*fats+6*protien;
-    cout<<"totalcalories="<<totalcalories;
-    
-}
